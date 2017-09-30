e3db7d44604fc4645d8e3f171abf30443a70cbfe
Gabriel Rohden <gabriel_rohden@outlook.com>
2017-09-30 18:20:14 -0300

    Added icomoon and its tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,23 @@
-const gulp      = require("gulp");
-const cache     = require("gulp-cached");
-const remember  = require("gulp-remember");
-const header    = require("gulp-header");
-const footer    = require("gulp-footer");
+const gulp = require("gulp");
+const cache = require("gulp-cached");
+const remember = require("gulp-remember");
+const header = require("gulp-header");
+const footer = require("gulp-footer");
 
 //Utils
 const browserSync = require('browser-sync').create();
-const bsConfigs   = require("./bs-config.js");
-const utils   = require("gulp-util");
-const log     = utils.log;
-const error   = utils.colors.red;
-const info    = utils.colors.magenta;
+const bsConfigs = require("./bs-config.js");
+const utils = require("gulp-util");
+const log = utils.log;
+const error = utils.colors.red;
+const info = utils.colors.magenta;
 const success = utils.colors.green;
 
-const templateCache   = require('gulp-angular-templatecache');
-const jshint          = require("gulp-jshint");
-const babel           = require("gulp-babel");
-const concat          = require("gulp-concat");
-const sass            = require("gulp-sass");
+const templateCache = require('gulp-angular-templatecache');
+const jshint = require("gulp-jshint");
+const babel = require("gulp-babel");
+const concat = require("gulp-concat");
+const sass = require("gulp-sass");
 
 
 
@@ -35,12 +35,12 @@ function compileScripts(event) {
   return gulp
     .src(paths.scripts.compile)
     .pipe(cache(paths.scripts.cacheName))
-      .pipe(jshint())
-      .pipe(jshint.reporter('jshint-stylish'))
-      .pipe(babel())
-        .on("error", logError)
-      .pipe(header("(function(){\n"))
-      .pipe(footer("\n}());"))
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(babel())
+    .on("error", logError)
+    .pipe(header("(function(){\n"))
+    .pipe(footer("\n}());"))
     .pipe(remember(paths.scripts.cacheName))
     .pipe(concat('app.js'))
     .pipe(gulp.dest(paths.scripts.build));
@@ -51,22 +51,28 @@ function logError(err) {
   this.emit('end');
 }
 
-function compileTemplates() { 
+function compileTemplates() {
   return gulp
     .src(paths.templates.compile)
-    .pipe(templateCache({standalone:true}))
+    .pipe(templateCache({ standalone: true }))
     .pipe(gulp.dest(paths.templates.build));
 }
 
-function compileSass() { 
+function compileSass() {
   return gulp
-  .src(paths.styles.compile)
-  .pipe(cache(paths.styles.cacheName))
+    .src(paths.styles.compile)
+    .pipe(cache(paths.styles.cacheName))
     .pipe(sass().on("error", sass.logError))
-  .pipe(remember(paths.styles.cacheName))
-  .pipe(concat('app.css'))
-  .pipe(gulp.dest(paths.styles.build));
+    .pipe(remember(paths.styles.cacheName))
+    .pipe(concat('app.css'))
+    .pipe(gulp.dest(paths.styles.build));
 
+}
+
+function copyIcons() {
+  return gulp
+    .src(paths.icons.source)
+    .pipe(gulp.dest(paths.icons.build));
 }
 
 function copyViews() {
@@ -77,28 +83,28 @@ function copyViews() {
 
 //FIXME: handle deletion for cache
 
-function watchScripts(){
+function watchScripts() {
   return gulp.watch(paths.scripts.compile, ['compile:scripts']);
 }
-  
-function watchTemplates(){
+
+function watchTemplates() {
   return gulp.watch(paths.templates.compile, ['compile:templates']);
 }
-  
-function watchViews(){
+
+function watchViews() {
   return gulp.watch(paths.views.compile, ['copy:views']);
 }
 
-function watchSass(){
+function watchSass() {
   return gulp.watch(paths.styles.compile, ['compile:sass']);
 }
 
 function initBrowserSync() {
-    browserSync.init(bsConfigs);
+  browserSync.init(bsConfigs);
 }
 
 //Clear all folders except the vendor ones (which are created by bower)
-function clearBuild() { 
+function clearBuild() {
   return del(paths.clean);
 }
 
@@ -114,7 +120,8 @@ gulp.task("compile", [
   "compile:scripts",
   "compile:sass",
   "compile:templates",
-  "copy:views"
+  "copy:views",
+  "copy:icons"
 ]);
 
 gulp.task("watch", [
@@ -135,9 +142,10 @@ gulp.task("compile:templates", compileTemplates);
 
 //Copies
 gulp.task("copy:views", copyViews);
+gulp.task("copy:icons",copyIcons);
 
 //Watchers
-gulp.task("watch:scripts",    watchScripts);
-gulp.task("watch:templates",  watchTemplates);
-gulp.task("watch:views",      watchViews);
-gulp.task("watch:sass",       watchSass);
+gulp.task("watch:scripts", watchScripts);
+gulp.task("watch:templates", watchTemplates);
+gulp.task("watch:views", watchViews);
+gulp.task("watch:sass", watchSass);
